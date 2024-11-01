9c467fc760689f735252f76675ffdf893ab85b8f
Sebastian Magnusson <sebbe.bebbe.11@gmail.com>
2024-11-01 14:04:17 +0100

    Changes to padding and titlesizes

--- rootlogon.C
+++ rootlogon.C
@@ -12,12 +12,14 @@ void rootlogon() {
       0);  // Let's make our plots look a bit better. Get rid of the stat box
   /*gStyle->SetOptTitle(
       0); */ // Get rid of the title (good plots use legends instead)
-  gStyle->SetTitleSize(0.05, "x");  // Use bigger text on the axes
-  gStyle->SetTitleSize(0.05, "y");
-  gStyle->SetLabelSize(0.05, "x");  // Use bigger labels too
-  gStyle->SetLabelSize(0.05, "y");
-  gStyle->SetPadTopMargin(0.065);  // Change the margins to fit our new sizes
+  gStyle->SetTitleFontSize(0.065);
+  gStyle->SetTitleSize(0.04, "x");
+
+  gStyle->SetTitleSize(0.04, "y");
+  gStyle->SetLabelSize(0.04, "x");  // Use bigger labels too
+  gStyle->SetLabelSize(0.04, "y");
+  gStyle->SetPadTopMargin(0.1);  // Change the margins to fit our new sizes
   gStyle->SetPadRightMargin(0.05);
-  gStyle->SetPadBottomMargin(0.16);
-  gStyle->SetPadLeftMargin(0.16);
+  gStyle->SetPadBottomMargin(0.10);
+  gStyle->SetPadLeftMargin(0.10);
 }
